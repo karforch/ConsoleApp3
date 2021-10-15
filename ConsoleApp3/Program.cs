d9e221d3ce1cc34adc92eaca661faff6faa82f4f
User <User@USER-PC>
2021-10-15 22:03:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Введите первое число: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Числа равны");
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else
+            {
+                Console.WriteLine("Второе число больше первого");
+            }
+            Console.ReadKey();
+        }
+    }
+}
